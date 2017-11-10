8e25d93589d5d6e2d8c93148664014ec442897b1
LEO-DESKTOP\User <leoinyvr@gmail.com>
2017-11-10 13:43:54 -0800

    can find uvc camera device and open after executed chmod 777 /dev/bus/usb/001/014

--- main.cpp
+++ main.cpp
@@ -87,8 +87,7 @@ int main(int argc, char *argv[])
       }
       puts("UVC initialized");
       /* Locates the first attached UVC device, stores in dev */
-//      res = uvc_find_device(ctx, &dev, 0, 0, NULL); /* filter devices: vendor_id, product_id, "serial_num" */
-      res = uvc_find_device2(ctx, &dev, 0, 0, NULL,0);
+      res = uvc_find_device(ctx, &dev, 0, 0, NULL); /* filter devices: vendor_id, product_id, "serial_num" */
       if (res < 0) {
         uvc_perror(res, "uvc_find_device"); /* no devices found */
       } else {
